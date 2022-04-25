b684659751f5df73fa6a2028e209e95e53843637
Liudas <repkovas@gmail.com>
2022-04-25 18:01:08 +0300

    1 pamoka

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Chat from './screens/Chat';
+
+const Stack = createStackNavigator();
+
+
+function ChatStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen component={Chat} name={'Chatas'}/>
+    </Stack.Navigator>
+  )
+}
+
+function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <ChatStack></ChatStack>
+    </NavigationContainer>
+  )
+}
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RootNavigator />
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
